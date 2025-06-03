1ea4b9beefe5e6371a9c0eb63b7ba5274dd51193
Carson Katri <Carson.katri@gmail.com>
2025-06-03 15:41:09 -0400

    Add topLevelAwait plugin to Vite config

--- example/vite.config.js
+++ example/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 import tailwindcss from '@tailwindcss/vite'
 
+import topLevelAwait from "vite-plugin-top-level-await"
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    fs: {
-      strict: false
-    }
-  }
+  plugins: [react(), tailwindcss(), topLevelAwait()],
+  optimizeDeps: {
+    exclude: ['@otp-interop/web-socket-dist']
+  },
 })
